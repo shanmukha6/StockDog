16cdeb3e0ea8d02b5d523111b345df5a3267bdd2
shanmukha <shanmukha@github.com>
2016-04-19 12:30:52 -0400

    Implemented WatchList Ctrl

--- app/scripts/controllers/watchlist.js
+++ app/scripts/controllers/watchlist.js
@@ -8,10 +8,34 @@
  * Controller of the stockDogApp
  */
 angular.module('stockDogApp')
-  .controller('WatchlistCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('WatchlistCtrl',
+    function ($scope,$routeParams, $modal,
+    WatchlistService, CompanyService) {
+
+      //Initializations
+    $scope.companies = CompanyService.query();
+      $scope.watchlist = WatchlistService.query($routeParams.listId);
+      $scope.stocks = $scope.watchlist.stocks;
+      $scope.newStock = {};
+
+      var addStockModal = $modal({
+        scope: $scope,
+        template: 'views/templates/addstock-modal.html',
+        show: false
+      });
+
+      //Expose showStockModal to view via $scope
+      $scope.showStockModal = function () {
+        addStockModal.$promise.then(addStockModal.show);
+      };
+
+      //Call the WatchListModel addStock() function and hide the element modal
+      $scope.addStock = function () {
+        $scope.watchlist.addStock({
+          listId: $routeParams.listId,
+          company: $scope.newStock.shares
+        });
+        addStockModal.hide();
+        $scope.newStock = {};
+      };
   });
