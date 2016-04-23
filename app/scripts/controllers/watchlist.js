77618226aaa228c0ffb05d3f44fff70eb890a17a
shanmukha <shanmukha@github.com>
2016-04-22 21:36:35 -0400

    Modified watchlist view

--- app/scripts/controllers/watchlist.js
+++ app/scripts/controllers/watchlist.js
@@ -8,34 +8,29 @@
  * Controller of the stockDogApp
  */
 angular.module('stockDogApp')
-  .controller('WatchlistCtrl',
-    function ($scope,$routeParams, $modal,
-    WatchlistService, CompanyService) {
-
-      //Initializations
+  .controller('WatchlistCtrl', function ($scope, $routeParams, $modal, WatchlistService, CompanyService) {
+    // Initializations
     $scope.companies = CompanyService.query();
-      $scope.watchlist = WatchlistService.query($routeParams.listId);
-      $scope.stocks = $scope.watchlist.stocks;
-      $scope.newStock = {};
+    $scope.watchlist = WatchlistService.query($routeParams.listId);
+    $scope.stocks = $scope.watchlist.stocks;
+    $scope.newStock = {};
+    var addStockModal = $modal({
+      scope: $scope,
+      template: 'views/templates/addstock-modal.html',
+      show: false
+    });
 
-      var addStockModal = $modal({
-        scope: $scope,
-        template: 'views/templates/addstock-modal.html',
-        show: false
+    $scope.showStockModal = function () {
+      addStockModal.$promise.then(addStockModal.show);
+    };
+
+    $scope.addStock = function () {
+      $scope.watchlist.addStock({
+        listId: $routeParams.listId,
+        company: $scope.newStock.company,
+        shares: $scope.newStock.shares
       });
-
-      //Expose showStockModal to view via $scope
-      $scope.showStockModal = function () {
-        addStockModal.$promise.then(addStockModal.show);
-      };
-
-      //Call the WatchListModel addStock() function and hide the element modal
-      $scope.addStock = function () {
-        $scope.watchlist.addStock({
-          listId: $routeParams.listId,
-          company: $scope.newStock.shares
-        });
-        addStockModal.hide();
-        $scope.newStock = {};
-      };
+      addStockModal.hide();
+      $scope.newStock = {};
+    };
   });
